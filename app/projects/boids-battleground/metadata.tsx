740b50d1806c81ce808fbefc4711ab4cf79c08c3
SollalF <64600280+SollalF@users.noreply.github.com>
2025-02-25 15:53:35 +0800

    Shadcn transition

--- app/projects/boids-battleground/metadata.tsx
+++
@@ -1,5 +0,0 @@
-export const metadata = {
-  title: 'Boids Battleground',
-  description: 'A game where you can battle against other players in a battleground.',
-  image: '/projects/boids-battleground/thumbnail.png',
-};
