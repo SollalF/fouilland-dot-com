1c6aba5875d3b907fa9a3695a5232d6d1cd777de
SollalF <64600280+SollalF@users.noreply.github.com>
2025-03-04 06:07:05 +0800

    Add Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,18 +1,54 @@
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Menu } from "lucide-react";
+"use client";
+
 import { Card } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Logo from "@/components/ui/logo";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
+
 const Navbar = () => {
   return (
-    <Card className="m-1 p-1">
+    <Card className="m-1 p-1 flex flex-row justify-between items-center sticky top-0">
       <Logo />
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Home
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/about" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                About Me
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/projects" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Projects
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/blog" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Blog
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <Button>Contact</Button>
     </Card>
   );
 };
--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
+import Link from "next/link";
 
 const Logo = () => {
   return (
-    <Image
-      src="/logo.jpg"
-      alt="logo"
-      className="rounded-full"
-      width={48}
-      height={48}
-    />
+    <Link href="/">
+      <Image
+        src="/logo.jpg"
+        alt="logo"
+        className="rounded-full"
+        width={48}
+        height={48}
+      />
+    </Link>
   );
 };
 
